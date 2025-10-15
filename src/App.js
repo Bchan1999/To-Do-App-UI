a6aa22468a9797f40efd35e3a396259cf3b67a63
Bchan1999 <brandon.nic99@gmail.com>
2025-10-14 20:58:02 -0600

    connect to backend

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, { use } from 'react';
 import './App.css';
 import ToDoList from './Components/ToDoList.js';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
 import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools'
+import { useQuery } from '@tanstack/react-query';
+
+const fetchTodo = async () => {
+  const res = await fetch('http://localhost:8080/todo')
+  if (!res.ok) throw new Error("Error fetching data")
+    
+  return res.json()
+}
 
 function App() {
-  //create a client
-  const queryClient = new QueryClient()
+  
+  //passes queries key for refetching and caching
+  const {data} = useQuery({queryKey: ["todo"], queryFn: fetchTodo})
 
+  var getItem = data[0]
+  {data?.map((todo) => {
+        console.log(todo)
+  })}
+
+  const listTest = data.map(todo => 
+    <li>{todo.header}</li>
+  )
   const [isOpen, setIsOpen] = React.useState(false)
 
   return (
-    <QueryClientProvider client={queryClient}>
+  <>
+      <ul>
+        {data.map(todo => 
+        <li>{todo.header}</li>
+        )}
+      </ul>
       <ToDoList/>
        <button
         onClick={() => setIsOpen(!isOpen)}
       >{`${isOpen ? 'Close' : 'Open'} the devtools panel`}</button>
       {isOpen && <ReactQueryDevtoolsPanel onClose={() => setIsOpen(false)} />}
-    </QueryClientProvider>
+  </>
   );
 }
 
