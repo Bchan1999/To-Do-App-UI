7e3e23dd6360d774ccf607db5dbb3fdd2dc65210
Bchan1999 <brandon.nic99@gmail.com>
2026-01-27 21:44:21 -0700

    Add loading state

--- src/App.js
+++ src/App.js
@@ -8,31 +8,27 @@ import { useQuery } from '@tanstack/react-query';
 const fetchTodo = async () => {
   const res = await fetch('http://localhost:8080/todo')
   if (!res.ok) throw new Error("Error fetching data")
-    
   return res.json()
 }
 
 function App() {
-  
   //passes queries key for refetching and caching
-  const {data} = useQuery({queryKey: ["todo"], queryFn: fetchTodo})
+  const {data, isLoading, error} = useQuery({
+    queryKey: ["todo"], 
+    queryFn: fetchTodo
+  })
 
-  var getItem = data[0]
-  {data?.map((todo) => {
-        console.log(todo)
-  })}
-
-  const listTest = data.map(todo => 
-    <li>{todo.header}</li>
-  )
   const [isOpen, setIsOpen] = React.useState(false)
 
+  if (isLoading) return <p>Loading...</p>
+  
+  if (error) return <p>Error occured: {error.message}</p>
   return (
   <>
       <ul>
-        {data.map(todo => 
+        {data.map((todo) => (
         <li>{todo.header}</li>
-        )}
+        ))}
       </ul>
       <ToDoList/>
        <button
